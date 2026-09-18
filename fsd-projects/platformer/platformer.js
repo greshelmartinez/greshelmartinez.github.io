71fb663e4c45c0582c393dbfec963f1adebd10a7
greshelmartinez <greshel.martinez@sarahtreed.org>
2026-09-18 18:29:57 +0000

    Finalized platformer game

--- fsd-projects/platformer/platformer.js
+++ fsd-projects/platformer/platformer.js
@@ -1,54 +1,81 @@
 $(function () {
-  // initialize canvas and context when able to
-  canvas = document.getElementById("canvas");
-  ctx = canvas.getContext("2d");
-  window.addEventListener("load", loadJson);
-
-  function setup() {
-    if (firstTimeSetup) {
-      halleImage = document.getElementById("player");
-      projectileImage = document.getElementById("projectile");
-      cannonImage = document.getElementById("cannon");
-      $(document).on("keydown", handleKeyDown);
-      $(document).on("keyup", handleKeyUp);
-      firstTimeSetup = false;
-      //start game
-      setInterval(main, 1000 / frameRate);
-    }
-
-    // Create walls - do not delete or modify this code
-    createPlatform(-50, -50, canvas.width + 100, 50); // top wall
-    createPlatform(-50, canvas.height - 10, canvas.width + 100, 200, "rgb(118, 0, 233)"); // bottom wall
-    createPlatform(-50, -50, 50, canvas.height + 500); // left wall
-    createPlatform(canvas.width, -50, 50, canvas.height + 100); // right wall
-
-    //////////////////////////////////
-    // ONLY CHANGE BELOW THIS POINT //
-    //////////////////////////////////
-
-    // TODO 1 - Enable the Grid
-    // toggleGrid();
+ // initialize canvas and context when able to
+ canvas = document.getElementById("canvas");
+ ctx = canvas.getContext("2d");
+ window.addEventListener("load", loadJson);
 
 
-    // TODO 2 - Create Platforms
+ function setup() {
+   if (firstTimeSetup) {
+     halleImage = document.getElementById("player");
+     projectileImage = document.getElementById("projectile");
+     cannonImage = document.getElementById("cannon");
+     $(document).on("keydown", handleKeyDown);
+     $(document).on("keyup", handleKeyUp);
+     firstTimeSetup = false;
+     //start game
+     setInterval(main, 1000 / frameRate);
+   }
+
+
+   // Create walls - do not delete or modify this code
+   createPlatform(-50, -50, canvas.width + 100, 50); // top wall
+   createPlatform(-50, canvas.height - 10, canvas.width + 100, 200, "rgb(118, 0, 233)"); // bottom wall
+   createPlatform(-50, -50, 50, canvas.height + 500); // left wall
+   createPlatform(canvas.width, -50, 50, canvas.height + 100); // right wall
+
+
+   //////////////////////////////////
+   // ONLY CHANGE BELOW THIS POINT //
+   //////////////////////////////////
+
+
+   // TODO 1 - Enable the Grid
+   toggleGrid();
+   
+
+
+   // TODO 2 - Create Platforms
+createPlatform(100, 600, 290, 20,)
+createPlatform(1400, 500, 50, 50, "red");
+createPlatform(500, 0, 20, 290);
+createPlatform(1350, 400, 50, 50, "red");
 
 
 
 
-    // TODO 3 - Create Collectables
 
 
 
-    
-    // TODO 4 - Create Cannons
+ 
 
 
-    
-    
-    //////////////////////////////////
-    // ONLY CHANGE ABOVE THIS POINT //
-    //////////////////////////////////
-  }
+   // TODO 3 - Create Collectables
+     createCollectable("steve", 1350, 50);
+createCollectable("diamond", 200, 170, 0.5, 0.7);
 
-  registerSetup(setup);
+
+
+
+
+
+  
+   // TODO 4 - Create Cannons
+     createCannon("top", 300, 750);
+createCannon("right", 300, 2000);
+
+
+
+
+  
+  
+   //////////////////////////////////
+   // ONLY CHANGE ABOVE THIS POINT //
+   //////////////////////////////////
+ }
+
+
+ registerSetup(setup);
 });
+
+
